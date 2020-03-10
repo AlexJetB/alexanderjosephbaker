6ac1c75e5bf2ded899038d34d20ee8c588c1a437
Alex Baker <baker.alex.j@gmail.com>
2020-03-09 22:28:50 -0600

    K

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var w09Route = require('./routes/assignments/w09');
+var w09Router = require('./routes/assignments/w09');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
